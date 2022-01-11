92dca80cab90da8e769dec383bb95afd803991af
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-01-11 10:11:29 +0100

    Add models

--- charity_donation_app/main/models.py
+++ charity_donation_app/main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    type_choices = (
+        (1, 'Fundacja'),
+        (2, 'Organizacja pozarządowa'),
+        (3, 'Zbiórka lokalna')
+    )
+    type = models.IntegerField(choices=type_choices, default=1)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=100)
+    pick_up_date = models.DateField()
+    pick_up_time = models.DateTimeField()
+    pick_up_comment = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
--- charity_donation_app/main/views.py
+++ charity_donation_app/main/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.views import View
 
-# Create your views here.
-
 
 class LandingPageView(View):
     def get(self, request):
