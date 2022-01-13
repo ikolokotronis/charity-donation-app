c9405dcb1b9ad73b40dad65f8eb326af57c97670
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-01-13 16:44:21 +0100

    Update admin site

--- charity_donation_app/main/admin.py
+++ charity_donation_app/main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from main.models import Category, Institution, Donation, InstitutionCategories
+from main.models import Category, Institution, Donation, InstitutionCategories, DonationCategories
 
 admin.site.register(Category)
 admin.site.register(Institution)
 admin.site.register(Donation)
 admin.site.register(InstitutionCategories)
+admin.site.register(DonationCategories)
