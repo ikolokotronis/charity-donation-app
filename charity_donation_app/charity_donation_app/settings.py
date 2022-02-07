ef0e04fd3d92ff846d145921430c17ca07253156
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-02-07 14:19:20 +0100

    Settings update

--- charity_donation_app/charity_donation_app/settings.py
+++ charity_donation_app/charity_donation_app/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'pl-pl'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Warsaw'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
